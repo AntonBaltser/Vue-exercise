abbe2125e62a72d47be0727553464c58aff34840
AntonBaltser <96978904+AntonBaltser@users.noreply.github.com>
2022-05-31 23:24:11 +0300

    ein bisschen weiter

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const App = {
     template: `   
     <fieldset>
       <legend>Übung:</legend>
-      <div v-for="select in selects">
+      <div id="form1" v-for="select in selects">
     <input
         ref="check"
         type="checkbox"
@@ -16,7 +16,26 @@ const App = {
       {{ select.text }}
     </label>
       </div>
-    </fieldset>`,
+    </fieldset>
+  <hr>
+  <br>
+  <fieldset>
+    <legend>Übung_2:</legend>
+    <div id="form2" v-for="select in selects1">
+      <input
+          ref="check1"
+          type="checkbox"
+          :value="select.text"
+          :key="select.id"
+          @click="addValue"
+      >
+      <label
+          :for="select.id"
+      >
+        {{ select.text }}
+      </label>
+    </div>
+  </fieldset>`,
 
     data(){
         return{
@@ -37,15 +56,33 @@ const App = {
                     creationDate: "2022-05-24 11:35:52"
                 }
             ],
+            selects1: [
+                {
+                    id: 338,
+                    text: "wie gehts?",
+                    creationDate: "2022-05-29 23:26:36"
+                },
+                {
+                    id: 341,
+                    text: "was hast du vor?",
+                    creationDate: "2022-05-30 21:28:39"
+                },
+                {
+                    id: 303,
+                    text: "https://www.youtube.com/watch?v=u4ENF5OCtkY",
+                    creationDate: "2022-05-24 11:35:52"
+                }
+            ],
         }
     },
     methods: {
-        addValue(input) {
-            this.$refs.check.forEach(input => input.checked = false)
-            input.target.checked = true
-            console.log(input.target.value)
+        addValue(memory1) {
+            if (memory1.target.parentElement.id === 'form1') this.$refs.check.forEach(input => input.checked = false)
+            else if (memory1.target.parentElement.id === 'form2') this.$refs.check1.forEach(input => input.checked = false)
+            memory1.target.checked = true
+            console.log(memory1.target.value)
         }
-    }
+    },
 }
 
 Vue.createApp(App).mount("#app")
