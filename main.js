50e48fc5a7ca1e6f7352d21a4c636c7dec84bddc
AntonBaltser <96978904+AntonBaltser@users.noreply.github.com>
2022-06-01 07:26:55 +0300

    no repeat yourself

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
 const App = {
     template: `   
-    <fieldset>
-      <legend>Übung:</legend>
-      <div id="form1" v-for="item in object_1">
-    <input
-        ref="check_1"
-        type="checkbox"
-        :value="item.text"
-        :key="item.id"
-        @click="addValue"
-    >
-    <label
-        :for="item.id"
-    >
-      {{ item.text }}
-    </label>
-      </div>
-    </fieldset>
-  <hr>
-  <br>
-  <fieldset>
-    <legend>Übung_2:</legend>
-    <div id="form2" v-for="item in object_2">
+  <fieldset v-for="(items, index) in main">
+    <legend>{{index}}</legend>
+    <div 
+    :id="index"
+     v-for="item in items"
+     >
       <input
-          ref="check_2"
+          :ref="index"
           type="checkbox"
           :value="item.text"
           :key="item.id"
@@ -35,65 +19,67 @@ const App = {
         {{ item.text }}
       </label>
     </div>
-  </fieldset>`,
-
+  </fieldset>
+`,
     data(){
         return{
-            object_1:[
-                {
-                    id: 338,
-                    text: "Hallo",
-                    creationDate: "2022-05-29 23:26:36"
-                },
-                {
-                    id: 341,
-                    text: "hallo welt",
-                    creationDate: "2022-05-30 21:28:39"
-                },
-                {
-                    id: 303,
-                    text: "https://youtu.be/gwDoRPcPxtc",
-                    creationDate: "2022-05-24 11:35:52"
-                }
-            ],
-            object_2: [
-                {
-                    id: 338,
-                    text: "wie gehts?",
-                    creationDate: "2022-05-29 23:26:36"
-                },
-                {
-                    id: 341,
-                    text: "was hast du vor?",
-                    creationDate: "2022-05-30 21:28:39"
-                },
-                {
-                    id: 303,
-                    text: "https://www.youtube.com/watch?v=u4ENF5OCtkY",
-                    creationDate: "2022-05-24 11:35:52"
-                }
-            ],
-            form1: null,
-            form2: null,
+            main: {
+                object_1: [
+                    {
+                        id: 338,
+                        text: "Hallo",
+                        creationDate: "2022-05-29 23:26:36"
+                    },
+                    {
+                        id: 341,
+                        text: "hallo welt",
+                        creationDate: "2022-05-30 21:28:39"
+                    },
+                    {
+                        id: 303,
+                        text: "https://youtu.be/gwDoRPcPxtc",
+                        creationDate: "2022-05-24 11:35:52"
+                    }
+                ],
+                object_2: [
+                    {
+                        id: 338,
+                        text: "wie gehts?",
+                        creationDate: "2022-05-29 23:26:36"
+                    },
+                    {
+                        id: 341,
+                        text: "was hast du vor?",
+                        creationDate: "2022-05-30 21:28:39"
+                    },
+                    {
+                        id: 303,
+                        text: "https://www.youtube.com/watch?v=u4ENF5OCtkY",
+                        creationDate: "2022-05-24 11:35:52"
+                    }
+                ]
+            }
+            ,
+            object_1: null,
+            object_2: null,
+            nameId: '',
+            valueItem: null
         }
     },
     methods: {
         addValue(select) {
-            if (select.target.parentElement.id === 'form1') this.$refs.check_1.forEach(input => {
+
+            this.nameId = select.target.parentElement.id
+            this.$refs[this.nameId].forEach(input => {
                 if (select.target.value === input.value)
-                    input.checked ? this.form1 = input.value : this.form1 = null
-                else {
-                    input.checked = false
-                }
+                    input.checked ? this.valueItem = input.value : this.valueItem = null
+                else input.checked = false
             })
-            else if (select.target.parentElement.id === 'form2') this.$refs.check_2.forEach(input => {
-                if (select.target.value === input.value)
-                    input.checked ? this.form2 = input.value : this.form2 = null
-                else {
-                    input.checked = false
-                }
-            })
-            console.log(this.form1, this.form2)
+            if(this.nameId === 'object_1') this.object_1 = this.valueItem
+            else if(this.nameId === 'object_2') this.object_2 = this.valueItem
+
+            console.log(this.object_1, this.object_2)
+
         }
     },
 }
