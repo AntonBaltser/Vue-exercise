4a22bd83cacb5b50610264a24c83f8820b9f1f7e
AntonBaltser <96978904+AntonBaltser@users.noreply.github.com>
2022-06-02 17:58:29 +0300

    so gefällt mir schon bestimmt besser)

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {reactive, ref} from "vue";
-const main = reactive( {
+
+const main = reactive({
   card_1: [
     {
       id: 1,
@@ -28,18 +29,46 @@ const main = reactive( {
       id: 3,
       text: "https://youtu.be/watch?v=u4ENF5OCtkY",
     }
-  ]
+  ], card_3: [
+    {
+      id: 1,
+      text: "Hallo",
+    },
+    {
+      id: 2,
+      text: "hallo welt",
+    },
+    {
+      id: 3,
+      text: "https://youtu.be/gwDoRPcPxtc",
+    }
+  ],
+  card_4: [
+    {
+      id: 1,
+      text: "wie gehts?",
+    },
+    {
+      id: 2,
+      text: "was hast du vor?",
+    },
+    {
+      id: 3,
+      text: "https://youtu.be/watch?v=u4ENF5OCtkY",
+    }
+  ],
 })
 let object = {
   card_1: [],
-  card_2: []
+  card_2: [],
+  card_3: [],
+  card_4: []
 }
-let object_1 = null
-let object_2 = null
-let valueItem = null
-
+const out = { }
 const inputs = ref(object)
 const addValue = (select) => {
+
+  let valueItem = null
 
   const nameId = select.target.parentElement.id
   inputs.value[nameId].forEach(input => {
@@ -47,49 +76,54 @@ const addValue = (select) => {
       input.checked ? valueItem = input.value : valueItem = null
     else input.checked = false
   })
-  if(nameId === 'card_1') object_1 = valueItem
-  else if(nameId === 'card_2')  object_2 =valueItem
+  if (nameId === 'card_1') out[nameId] = valueItem
+  else if (nameId === 'card_2') out[nameId] = valueItem
+  else if (nameId === 'card_3') out[nameId] = valueItem
+  else if (nameId === 'card_4') out[nameId] = valueItem
   inputs.value[nameId] = []
-
-  console.log(object_1, object_2)
+  
+  console.log(out)
 }
 </script>
 <template>
   <v-container>
     <div class="text-subtitle-4 text-h4 text-center">Cards with value</div>
     <v-layout class="d-block d-md-flex">
-    <v-card
-        color="#9575CD"
-        elevation="15"
-        class="mx-auto ma-3 text-white rounded-lg"
-        v-for="(items, indexName) in main"
-        width="400"
-    >
-      <v-card-header>
-        <v-card-header-text>
-          <v-card-title
-              class="justify-center">
-            {{indexName}}
-          </v-card-title>
-        </v-card-header-text>
-      </v-card-header>
-      <v-card-text
-          v-for="(item, index) in items"
-      >
-        <label :id="indexName">
-        <input
-            :class="item.id + (indexName === 'card_1' ? ' filled-in' : '')"
-            :ref="el => { if (el) inputs[indexName][index] = el }"
-            type="checkbox"
-            :value="item.text"
-            @click="addValue"
+      <v-row cols="6" class="mx-1">
+        <v-card
+            color="#9575CD"
+            elevation="15"
+            class="mx-auto ma-3 text-white rounded-lg"
+            v-for="(items, indexName) in main"
+            width="400"
         >
-          <span class="text-white">
+          <v-card-header>
+            <v-card-header-text>
+              <v-card-title
+                  class="justify-center">
+                {{ indexName }}
+              </v-card-title>
+            </v-card-header-text>
+          </v-card-header>
+          <v-card-text
+              v-for="(item, index) in items"
+          >
+            <label :id="indexName">
+              <input
+                  :class="item.id + (indexName === 'card_1' || indexName === 'card_4' ? ' filled-in' : '')"
+                  :ref="el => { if (el) inputs[indexName][index] = el }"
+                  type="checkbox"
+                  :value="item.text"
+                  @click="addValue"
+              >
+              <span class="text-white">
             {{ item.text }}
           </span>
-        </label>
-      </v-card-text>
-    </v-card>
+            </label>
+          </v-card-text>
+        </v-card>
+      </v-row>
     </v-layout>
+
   </v-container>
 </template>
